b8fe17dfd0c7dae072035933777b77d7c2583104
tarek khaled <tarekt820@gmail.com>
2018-12-30 15:02:28 +0200

    finish design for index.php

---
+++ public/includes/navbar.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark navbar-sec">
+  <a class="navbar-brand" href="#">IntCore</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home   </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="signup.php">Signup</a>
+      </li>
+    </ul>
+  </div>
+</nav>
--- public/index.php
+++ public/index.php
@@ -25,12 +25,56 @@
 </head>
 <body>
 
-<!-- Place to include navbar.php -->
-<?php require "includes/navbar.php"?>
+    <header id="header-sec">
+        <!-- Place to include navbar.php -->
+        <?php require "includes/navbar.php"?> 
 
+        <div class="overlay-header">
+            <!-- Content for this page : form to login   -->
+            <form action="profile.php" method="POST" id="form-sec">
+                <div class="container form-container">
+                    <div class="row">
+                        <div class="col-md-3 ">
+                            <div class='login-form'>
+                                <!-- part of image -->
+                                <div class="img-user">
+                                    <img src="images/icon.png" alt="user-icon">
+                                </div>
 
-<!-- Content for this page -->
+                                <!-- part of inputs -->
+                                <div class="item-login">
+                                    <div class="icon-item">
+                                        <i class="fa fa-user"></i>
+                                    </div>
+                                    <input class="input-item pwd-input" type="text" name="username" placeholder="username..">
+                                </div>
 
+                                <div class="item-login">
+                                    <div class="icon-item">
+                                        <i class="fa fa-unlock-alt"></i>
+                                    </div>
+                                    <input class="input-item pwd-input" type="password" name="password" placeholder="password..">
+                                </div>
+
+                                <label class="buttons-form">
+                                    <input type="checkbox" name="remember-me">  remember me
+                                </label>
+
+                                <div class="item-submit">
+                                    <input type="submit" value="login">
+                                </div>
+                        
+                        
+                            </div>
+                        </div>
+                    </div>
+                </div>          
+            </form>
+        </div>
+
+    </header>
+
+   
 
 <!-- Place to include footer.php -->
 <?php require "includes/footer.php" ?>
